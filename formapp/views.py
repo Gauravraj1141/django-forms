7c439e656ced3662e20c3882932f382bab8cf48f
Gaurav Rajput <105104456+Gauravraj1141@users.noreply.github.com>
2022-12-01 16:23:25 +0530

    second addition

--- formapp/views.py
+++ formapp/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from .forms import Registrationform
 # Create your views here.
 
 
-def home(request):
-    fm = Registrationform()
+def success(request):
+    return render(request, "formapp/success.html")
 
+
+def home(request):
+    if request.method == "POST":
+        fmdata = Registrationform(request.POST)
+        if fmdata.is_valid():
+            print("validate")
+            print(fmdata.cleaned_data)
+            print(fmdata.cleaned_data["Name"])
+            print(fmdata.cleaned_data["Email"])
+            print(fmdata.cleaned_data["Phone"])
+            print(fmdata.cleaned_data["division"])
+            return HttpResponseRedirect("/success")
+    else:
+        fm = Registrationform()
     return render(request, 'formapp/form.html', {"fms": fm})
